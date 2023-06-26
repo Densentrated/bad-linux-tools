14a41563a8e0bcbae7c1cd9c9951af22012b189f
Densentrated <72484574+Densentrated@users.noreply.github.com>
2023-06-26 15:26:32 -0700

    fixed code

--- badcat.rs
+++ badcat.rs
@@ -1,7 +1,7 @@
-use fs;
+use std::fs;
 use std::io::stdin;
 
 fn main() {
-    let fileName: str = std::io::stdin().read_line();
-    let fileText: str = fs::read_to_string("filename").unwrap();
+    let mut line = String::new();   
+    let fileName = std::io::stdin().read_line(&mut line).unwrap();
 }
