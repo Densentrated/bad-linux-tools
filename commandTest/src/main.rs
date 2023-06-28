fea9ada25723542d994d3f09136a257f474feabc
Densentrated <72484574+Densentrated@users.noreply.github.com>
2023-06-28 00:20:35 -0700

    tried to import things

--- commandTest/src/main.rs
+++ commandTest/src/main.rs
@@ -1,18 +1,16 @@
+#![allow(unused)]
+
 use clap::Parser;
 
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
-    _pattern: String,
-    _file_path: std::path::PathBuf,
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
 }
 
 fn main() {
-    println!("Hello, world!");
-    let _pattern = std::env::args().nth(1).expect("no pattern given");
-    let _file_path = std::env::args().nth(2).expect("no filepath given");
-    let _arugments = Cli {
-        _pattern: _pattern,
-        _file_path: std::path::PathBuf::from(_file_path),
-    };
+    let args = Cli::parse();
 }
-
-
