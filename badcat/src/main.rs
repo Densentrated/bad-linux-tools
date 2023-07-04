1a1bf37bb1aff2e2c1f4c646c852160ec983de76
Densentrated <72484574+Densentrated@users.noreply.github.com>
2023-07-04 00:43:26 -0700

    replaces th with tf

--- badcat/src/main.rs
+++ badcat/src/main.rs
@@ -11,10 +11,22 @@ fn main() {
     let mut content: String = std::fs::read_to_string(&args.filepath).expect("could not read file");
 
     // replaces "l and r" characters with "w" characters
-    let content: String = content.replace("l", "w");
-    let content: String = content.replace("r", "w");
-    println!("{}", content);
-
-    // replaces "th" with "ff"
+    content= content.replace("l", "w");
+    content = content.replace("r", "w");
     
+    // replaces "th" with "ff"
+    let mut i = 0;
+    while i < content.len() {
+        if (content.chars().nth(i) == Some('t')){
+            println!("t Character!");
+            if (content.chars().nth(i+1) == Some('h') || content.chars().nth(i+1) == Some('H')){
+                println!("h Character!");
+                content.replace_range(i..i+2, "ff");
+            }
+        }
+        i += 1;
+    }
+
+    // prints the changed content
+    println!("{}", content);   
 }
